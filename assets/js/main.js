a5a6942903a250b217691ab782ba5b2e30f11948
Sara <38770404+SaraDrwish@users.noreply.github.com>
2022-06-21 23:15:19 +0300

    footer and 6th

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,5 +27,43 @@ $(window).scroll(function() {
 
 
 
+
+
+
+
+// ------------------------------------------------------------------------------brand start
+
+
+$('.sec11').owlCarousel({
+    loop:true,
+    margin:25,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+
+// -------------------------------------------------------------------------------------------------
+
+
+
+
 }
 )
+
+
+
+
