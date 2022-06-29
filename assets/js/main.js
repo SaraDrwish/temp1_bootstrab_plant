1baf15c30c20968c38bffedbb6fa80cba1c8959a
Sara <38770404+SaraDrwish@users.noreply.github.com>
2022-06-29 15:47:50 +0300

    ed sl

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,3 +67,43 @@ $('.sec11').owlCarousel({
 
 
 
+// ------------------------------------LOADER
+
+let filterBtn = document.querySelectorAll(".filterBtn li");
+let items = document.querySelectorAll(".containerItems .items");
+
+filterBtn.forEach(function(el){   
+    el.addEventListener("click" , function(){
+         document.querySelector("ul li.active").classList.remove("active");
+        el.classList.add("active");
+
+        let target = el.dataset.target;
+
+          items.forEach(function(elmnt){
+
+            elmnt.classList.add("hiddin");
+            elmnt.classList.remove("visble");
+
+             
+            if(elmnt.dataset.id == target  || target== "all"){
+                elmnt.classList.add("visble");
+                elmnt.classList.remove("hiddin");
+            }
+
+        })
+
+    })
+} )
+
+function load(){
+    let loader = document.querySelector(".laoder");
+    loader.classList.add("fadOut");
+}
+
+function fade(){
+    setInterval(load , 3000);
+}
+fade()
+
+// -----------------------------------------------END LOADER
+
